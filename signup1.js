16412468b599c0f50adff342c5a190d367203d7d
Placide123 <placidetwiringiyimana@outlook.com>
2022-03-28 11:56:00 +0200

    third

---
+++ signup1.js
@@ -0,0 +1,74 @@
+let firstname=document.getElementById("txtFirstname");
+let lastname=document.getElementById("txtLastname");
+let email=document.getElementById("txtEmail");
+let pwd=document.getElementById("txtPwd");
+let conPwd=document.getElementById("txtConPwd");
+let form=document.querySelector("form");
+
+function validateInput(){
+    //check firstname is empty 
+    if(firstname.value.trim()===""){
+       onError(firstname,"firstname cannot be empty");
+    }else{
+        onSuccess(firstname);
+    }
+    
+    if(lastname.value.trim()===""){
+        onError(lastname,"Second Name can not be empty");
+    }else{
+        onSuccess(lastname);
+    }
+
+    if(email.value.trim()===""){
+        onError(email,"Email cannot be empty");
+    }else{
+        if(!isValidEmail(email.value.trim())){
+            onError(email,"Email is not valid");
+        }else{
+            onSuccess(email);
+        }
+    }
+
+    //password
+    if(pwd.value.trim()===""){
+        onError(pwd,"Password cannot be empty");
+     }else{
+         onSuccess(pwd);
+     }
+     if(conPwd.value.trim()===""){
+        onError(conPwd," Confirm Password cannot be empty");
+     }else{
+         if(pwd.value.trim()!==conPwd.value.trim()){
+            onError(conPwd,"Password & Confirm password not matching");
+         }
+         else
+         onSuccess(conPwd);
+     }
+}
+
+document.querySelector("button")
+.addEventListener("click",(event)=>{
+    event.preventDefault();
+    validateInput();
+});
+
+function onSuccess(input){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="hidden"; 
+    parent.classList.remove("error");
+    parent.classList.add("success");  
+}
+function onError(input,message){
+    let parent=input.parentElement;
+    let messageEle=parent.querySelector("small");
+    messageEle.style.visibility="visible";
+    messageEle.innerText=message;  
+    parent.classList.add("error");
+    parent.classList.remove("success");
+
+}
+
+function isValidEmail(email){
+   return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
